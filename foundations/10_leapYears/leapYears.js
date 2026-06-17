6690312cba007848c7d0d9f27b190a2e81780910
Jade <ch.jade427@gmail.com>
2026-06-17 10:58:31 -0700

    Finish exercises 6 - 11

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  const divisbleBy4 = year % 4 === 0;
+  const divisibleBy100 = year % 100 === 0;
+  const divisbleBy400 = year % 400 === 0;
 
+  return divisbleBy4 && (!divisibleBy100 || divisbleBy400);
 };
 
 // Do not edit below this line
