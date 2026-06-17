696f9da98935f4e10e55f56c67f35a66d29b5552
Jade <ch.jade427@gmail.com>
2026-06-17 11:13:18 -0700

    Pass multiply test

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -10,7 +10,9 @@ const sum = function (arr) {
   return arr.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function () {};
+const multiply = function (arr) {
+  return arr.reduce((prev, curr) => prev * curr, 1);
+};
 
 const power = function () {};
 
