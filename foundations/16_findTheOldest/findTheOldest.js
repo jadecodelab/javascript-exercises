5496853cb7af7bf71384209ada2e1bcf982b8090
Jade <ch.jade427@gmail.com>
2026-06-17 12:31:59 -0700

    Pass find oldest test

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  const currYear = new Date().getFullYear();
 
+  return people.reduce((oldest, person) => {
+    const oldestAge = (oldest.yearOfDeath || currYear) - oldest.yearOfBirth;
+    const personAge = (person.yearOfDeath || currYear) - person.yearOfBirth;
+
+    return personAge > oldestAge ? person : oldest;
+  });
 };
 
 // Do not edit below this line
