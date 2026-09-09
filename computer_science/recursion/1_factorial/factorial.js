30a3875ab6093c3273726b8575e34ece6e78818b
Jade <ch.jade427@gmail.com>
2026-09-09 09:21:27 -0700

    pass factorial tests:    √ 4th factorial     √ 6th factorial     √ 10th factorial     √ 15th factorial     √ 18th factorial     √ 0th factorial

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,6 +1,12 @@
-const factorial = function() {
+const factorial = function (n) {
+  if (n === 0) {
+    return 1;
+  }
 
+  return n * factorial(n - 1);
 };
 
 // Do not edit below this line
 module.exports = factorial;
+
+// npm test factorial.spec.js
