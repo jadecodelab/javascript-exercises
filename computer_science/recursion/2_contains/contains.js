fa66c620f89822a24463e2e41ff50d02a0d36116
Jade <ch.jade427@gmail.com>
2026-09-09 10:09:19 -0700

    Pass true if the provided number is a value within the object

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,18 @@
-const contains = function() {
-  
+const contains = function (obj, target) {
+  for (const value of Object.values(obj)) {
+    if (value === target) {
+      return true;
+    }
+
+    if (typeof value === "object") {
+      if (contains(value, target)) return true;
+    }
+  }
+
+  return false;
 };
-  
+
 // Do not edit below this line
 module.exports = contains;
+
+// npm test contains.spec.js
