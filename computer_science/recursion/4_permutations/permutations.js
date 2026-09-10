35b2acbd343837b4c10998a1f7aa6c131a48c875
Jade <ch.jade427@gmail.com>
2026-09-10 10:03:01 -0700

    Pass all permutations tests

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,6 +1,21 @@
-const permutations = function() {
-  
+const permutations = function (array, index = 0, results = []) {
+  if (index === array.length) {
+    results.push([...array]);
+    return results;
+  }
+
+  for (let i = index; i < array.length; i++) {
+    [array[index], array[i]] = [array[i], array[index]];
+
+    permutations(array, index + 1, results);
+
+    [array[index], array[i]] = [array[i], array[index]];
+  }
+
+  return results;
 };
-  
+
 // Do not edit below this line
 module.exports = permutations;
+
+//npm test permutations.spec.js
