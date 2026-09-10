9a7b60e106c3591b348f7b5aeb5c1f9d2f563efe
Jade <ch.jade427@gmail.com>
2026-09-09 17:36:46 -0700

    Pass all totalIntergers tests

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,26 @@
-const totalIntegers = function() {
-  
+const isObject = (value) => typeof value === "object" && value != null;
+
+const totalIntegers = function (obj) {
+  let count = 0;
+
+  if (!isObject(obj)) {
+    return;
+  }
+
+  const elements = Object.values(obj);
+
+  for (const element of elements) {
+    if (Number.isInteger(element)) {
+      count++;
+    } else if (isObject(element)) {
+      count += totalIntegers(element);
+    }
+  }
+
+  return count;
 };
-  
+
 // Do not edit below this line
 module.exports = totalIntegers;
+
+//npm test totalIntegers.spec.js
