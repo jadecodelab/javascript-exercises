e8a62b7ca1db3e26f86f3e5d8fb525956922a7af
Jade <ch.jade427@gmail.com>
2026-09-10 21:27:31 -0700

    Pass all pascal tests

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,6 +1,23 @@
-const pascal = function() {
-  
+const pascal = function (n) {
+  if (n === 1) return [1];
+
+  const previousRow = pascal(n - 1);
+
+  // add imaginary 0 to the start and end
+  const previousRowWithZeros = [0, ...previousRow, 0];
+
+  const newRow = [];
+
+  for (let i = 0; i < previousRowWithZeros.length - 1; i++) {
+    const leftNumber = previousRowWithZeros[i];
+    const rightNumber = previousRowWithZeros[i + 1];
+    newRow.push(leftNumber + rightNumber);
+  }
+
+  return newRow;
 };
-  
+
 // Do not edit below this line
 module.exports = pascal;
+
+// npm test pascal.spec.js
